d7f781744aedde6ec0c4522a7b9e41fba2b10aa8
Jeith Carrillo <jeith2@gmail.com>
2026-01-08 09:19:31 -0500

    feat: livewire duplicated. but is ok

--- resources/views/orchid/admin-dashboard-wrapper.blade.php
+++ resources/views/orchid/admin-dashboard-wrapper.blade.php
@@ -1,22 +1,3 @@
 <div id="livewire-dashboard-container">
     @livewire('admin-dashboard')
 </div>
-
-<script>
-    // Reinicializar Livewire cuando Turbo carga la página
-    document.addEventListener('turbo:load', function() {
-        // Esperar un momento para que Livewire se inicialice
-        setTimeout(function() {
-            if (window.Livewire) {
-                // Intentar métodos compatibles sin causar errores
-                try {
-                    if (typeof window.Livewire.restart === 'function') {
-                        window.Livewire.restart();
-                    }
-                } catch(e) {
-                    console.log('Livewire restart not available:', e);
-                }
-            }
-        }, 100);
-    });
-</script>
